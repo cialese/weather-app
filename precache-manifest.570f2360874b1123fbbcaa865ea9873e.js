b0cb359fd7f40be07758679c40ebc25065e37b9c
cinthyasegura <cinthyaless@gmail.com>
2019-07-22 19:49:25 -0500

    Updates

---
+++ precache-manifest.570f2360874b1123fbbcaa865ea9873e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e25398b44b704db63057016b425a448",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "928db79e5af2e2455f01",
+    "url": "/weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "07c92edafb9f39e0d16c",
+    "url": "/weather-app/static/css/main.55497536.chunk.css"
+  },
+  {
+    "revision": "928db79e5af2e2455f01",
+    "url": "/weather-app/static/js/2.9b3ead1c.chunk.js"
+  },
+  {
+    "revision": "07c92edafb9f39e0d16c",
+    "url": "/weather-app/static/js/main.d7c3ecd3.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "e2956e249a334f84bd512d87fa6368f6",
+    "url": "/weather-app/static/media/weather.e2956e24.JPEG"
+  }
+]);
