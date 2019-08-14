dc4cabacba8f0b187875af6698947e04503fb5b1
cinthyasegura <cinthyaless@gmail.com>
2019-08-13 22:38:36 -0500

    updating env variable

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     const country = e.target.elements.country.value;
     try {
       const API = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${
           process.env.REACT_APP_WEATHER_API_KEY
         }&units=metric`
       );
